379cd3d6adbfd727a492b13678bbca20fbe0924f
gajananbhusare <142707765+gajananbhusare@users.noreply.github.com>
2025-04-05 08:38:14 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 print("Hello")
 print("Welcome to Google Cloud")
 print("how r u?")
+print("Welcome to Google Cloud")
+print("Welcome to Google Cloud")
+print("Welcome to Google Cloud")
+print("Welcome to Google Cloud")
+print("Welcome to Google Cloud")
+print("Welcome to Google Cloud")
+
